77d9b36f1d671bf92819b603d27dabe0274220c7
pluresideas <26636262+pluresideas@users.noreply.github.com>
2019-10-18 12:33:49 -0500

    added Spring dependencies

---
+++ src/main/java/com/pluresidea/api/emergencyincident/ApiEmergencyIncidentApplication.java
@@ -0,0 +1,13 @@
+package com.pluresidea.api.emergencyincident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmergencyIncidentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEmergencyIncidentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluresidea/api/emergencyincident/ApiEmergencyIncidentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluresidea.api.emergencyincident;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEmergencyIncidentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
